721d8b43915bbf0e1d663024352c8f2f61e9555e
Mugisha Serein <152311662+mugisha-serein@users.noreply.github.com>
2026-07-09 11:49:10 +0200

    Add portfolio caption update command

--- application/vendors/portfolio_media_commands.py
+++ application/vendors/portfolio_media_commands.py
@@ -48,3 +48,18 @@ class MarkPortfolioMediaUploadedCommand:
         object.__setattr__(self, "vendor_id", _coerce_uuid(self.vendor_id, "vendor_id"))
         object.__setattr__(self, "media_id", _coerce_uuid(self.media_id, "media_id"))
         object.__setattr__(self, "expected_version", _coerce_expected_version(self.expected_version))
+
+
+@dataclass(frozen=True)
+class UpdatePortfolioCaptionCommand:
+    actor: AuthenticatedActor
+    vendor_id: uuid.UUID
+    media_id: uuid.UUID
+    expected_version: int
+    caption: str | None
+
+    def __post_init__(self) -> None:
+        object.__setattr__(self, "actor", _coerce_actor(self.actor))
+        object.__setattr__(self, "vendor_id", _coerce_uuid(self.vendor_id, "vendor_id"))
+        object.__setattr__(self, "media_id", _coerce_uuid(self.media_id, "media_id"))
+        object.__setattr__(self, "expected_version", _coerce_expected_version(self.expected_version))
