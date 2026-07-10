c350dab41933ab6dadd30237d75a9c5ec3687a28
Mugisha Serein <152311662+mugisha-serein@users.noreply.github.com>
2026-07-10 12:32:39 +0200

    fix(vendors): repair inquiry abuse migration syntax

--- django_app/vendors/migrations/0015_inquiry_abuse_record.py
+++ django_app/vendors/migrations/0015_inquiry_abuse_record.py
@@ -13,4 +13,52 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name="InquiryAbuseRecord",
             fields=[
-                ("
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("requester_identity", models.UUIDField(db_index=True)),
+                ("payload_digest", models.CharField(max_length=64)),
+                ("duplicate_window_key", models.BigIntegerField()),
+                (
+                    "created_at",
+                    models.DateTimeField(
+                        db_index=True,
+                        default=django.utils.timezone.now,
+                    ),
+                ),
+                (
+                    "vendor",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="inquiry_abuse_records",
+                        to="vendors.vendorprofile",
+                    ),
+                ),
+            ],
+            options={
+                "indexes": [
+                    models.Index(
+                        fields=["requester_identity", "vendor", "created_at"],
+                        name="vendors_inquiry_abuse_rate_idx",
+                    ),
+                ],
+                "constraints": [
+                    models.UniqueConstraint(
+                        fields=(
+                            "requester_identity",
+                            "vendor",
+                            "payload_digest",
+                            "duplicate_window_key",
+                        ),
+                        name="vendors_inquiry_abuse_duplicate_window_unique",
+                    ),
+                ],
+            },
+        ),
+    ]
